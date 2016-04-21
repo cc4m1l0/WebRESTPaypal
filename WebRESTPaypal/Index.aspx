872b502f80d704fb290f77de518f4f0468c25ead
Christian Barrero <christian_barrero@hotmail.com>
2016-04-21 10:36:59 -0500

    Add project files.

---
+++ WebRESTPaypal/Index.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebRESTPaypal.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>REST HATEOAS</title>
+    <style>
+        .hidden {
+            display: none; 
+
+        }
+    </style>
+    <script>
+        function mostrarResponse()
+        {
+            var btnver = document.getElementById("btnVerocultarresponse");
+            if (btnver.firstChild.data == "VER RESPONSE") {
+                var element = document.getElementById("lbResponse");
+                element.classList.remove("hidden");
+                btnver.firstChild.data = "OCULTAR RESPONSE";
+            }
+            else {
+                var element = document.getElementById("lbResponse");
+                element.classList.add("hidden");
+                btnver.firstChild.data = "VER RESPONSE";
+            }
+        }
+        
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <center>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="updatepanel" runat="server">
+            <ContentTemplate>
+                <div style="text-align: center;">
+                        <asp:Label ID="lbMetodo" runat="server" Text="..." Font-Size="Large"></asp:Label>
+                        <br/>
+                        <asp:Label ID="lbTarea" runat="server" Text="..." Font-Size="Large"></asp:Label>
+                        <br/>
+                        <button id="btnVerocultarresponse" type="button" onclick="mostrarResponse()">VER RESPONSE</button>
+                        <br/>
+                        <asp:Label ID="lbResponse" runat="server" Text="..." CssClass="hidden" ></asp:Label>
+                        <br/>
+                        <br/>
+                        <asp:Label ID="Label1" runat="server" Text="Lista de HATEOAS en el Response"></asp:Label>
+                        <br/>
+                        <center>
+                        <asp:ListView ID="lvLinks" runat="server" OnItemCommand="lvLinks_ItemCommand" >
+                            <LayoutTemplate>
+                                <table runat="server" class="table table-condensed" id="tblItems" style="border:0px solid black;"  >
+                                <tr runat="server"  >
+                                    <th runat="server" style="background:#ffd800;" >HREF</th>
+                                    <th runat="server" style="background:#ffd800;">REL</th>
+                                    <th runat="server" style="background:#ffd800;">METHOD</th>
+                                </tr>
+                                <tr runat="server" id="itemPlaceholder" />
+                                </table>
+                            </LayoutTemplate>
+                            <ItemTemplate>
+                                <tr runat="server" style="border-bottom:1px solid black;">
+                                <td style="border-bottom:1px solid black;">
+                                  <asp:Label ID="href" runat="Server" Text='<%#Eval("href") %>' />
+                                </td>
+                                <td style="border-bottom:1px solid black;">
+                                  <asp:Label ID="rel" runat="Server" Text='<%#Eval("rel") %>' />
+                                </td>
+                                <td style="border-bottom:1px solid black;">
+                                  <asp:LinkButton ID="LinkButton2" runat="Server" Text='<%#Eval("method") %>' CommandName="Execute" CommandArgument='<%#Eval("href").ToString()+","+Eval("rel")+","+Eval("method")%>' />
+                                </td>
+                              </tr>
+                            </ItemTemplate>
+                        </asp:ListView>
+                        </center>
+                        <br />
+                </div>
+            </ContentTemplate>
+            <Triggers>
+                <asp:PostBackTrigger ControlID="lvLinks" />
+            </Triggers>
+        </asp:UpdatePanel>
+        <asp:Label ID="lbEstado" runat="server" Text="" Font-Size="X-Large" ForeColor="#33CC33" Visible="false"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnCrearTarjeta" runat="server" Text="CREAR TARJETA" Visible="false" OnClick="btnCrearTarjeta_Click" />
+        </center>
+    </form>
+</body>
+</html>
---
+++ WebRESTPaypal/Index.aspx.cs
@@ -0,0 +1,146 @@
+﻿using Newtonsoft.Json.Linq;
+using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebRESTPaypal.Helpers;
+
+namespace WebRESTPaypal
+{
+    public partial class Index : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                CreateNewCard();
+            }
+        }
+
+        private void CreateNewCard()
+        {
+            lbEstado.Visible = false;
+            btnCrearTarjeta.Visible = false;
+            // ### Api Context
+            // Pass in a `APIContext` object to authenticate 
+            // the call and to send a unique request id 
+            // (that ensures idempotency). The SDK generates
+            // a request id if you do not pass one explicitly. 
+            // See [Configuration.cs](/Source/Configuration.html) to know more about APIContext.
+            var apiContext = Configuration.GetAPIContext();
+
+            lbMetodo.Text = "Método ejecutado: Create de la clase CreditCard";
+            lbTarea.Text = "Tarea: Crear una nueva tarjeta de crédito";
+            // A resource representing a credit card that can be used to fund a payment.
+            var card = new CreditCard()
+            {
+                expire_month = 11,
+                expire_year = 2018,
+                number = "4877274905927862",
+                type = "visa",
+                cvv2 = "874"
+            };
+            // Creates the credit card as a resource in the PayPal vault. The response contains an 'id' that you can use to refer to it in the future payments.
+            var cd = card.Create(apiContext);
+
+            var linksresponse = cd.links;
+            lvLinks.DataSource = linksresponse;
+            lvLinks.DataBind();
+
+            lbResponse.Text = cd.ConvertToJson();
+        }
+
+        protected void lvLinks_ItemCommand(object sender, ListViewCommandEventArgs e)
+        {
+            if(e.CommandName == "Execute")
+            {
+                string data = e.CommandArgument.ToString();
+                if (data != "")
+                {
+                    CreateNewRequest(data);
+                }
+            }
+        }
+
+        private void CreateNewRequest(string data)
+        {
+            string[] commandArgs = data.Split(new char[] { ',' });
+            Links l = new Links();
+            l.href = commandArgs[0];
+            l.rel = commandArgs[1];
+            l.method = commandArgs[2];
+
+            var apiContext = Configuration.GetAPIContext();
+
+
+            lbMetodo.Text = "Método ejecutado: " + l.method + " de la clase CreditCard";
+            lbTarea.Text = "Tarea: " + l.method.ToLower() + " tarjeta de crédito";
+
+            string url = l.href;
+            try
+            {
+                WebRequest myReq = WebRequest.Create(url);
+                myReq.ContentType = "application/json";
+                myReq.Method = l.method;
+                CredentialCache mycache = new CredentialCache();
+                myReq.Headers["Authorization"] = apiContext.AccessToken;
+                WebResponse wr = myReq.GetResponse();
+                HttpWebResponse httpResponse = (HttpWebResponse)wr;
+
+                if (httpResponse.StatusCode.ToString() == "OK")
+                {
+                    Stream receiveStream = wr.GetResponseStream();
+
+                    StreamReader reader = new StreamReader(receiveStream, Encoding.UTF8);
+                    string content = reader.ReadToEnd();
+                    Console.WriteLine(content);
+                    var json = "[" + content + "]"; // change this to array
+                    var objects = JArray.Parse(json); // parse as array  
+                    foreach (JObject o in objects.Children<JObject>())
+                    {
+                        foreach (JProperty p in o.Properties())
+                        {
+                            string name = p.Name;
+                            string value = p.Value.ToString();
+                            Console.Write(name + ": " + value);
+                        }
+                    }
+                    lbResponse.Text = json;
+                }
+
+                else if (httpResponse.StatusCode.ToString() == "NoContent")
+                {
+                    var linksresponse = new List<Links>();
+                    lvLinks.DataSource = linksresponse;
+                    lvLinks.DataBind();
+                    lbResponse.Text = "Tarjeta eliminada exitosamente";
+                    lbEstado.Text = "Tarjeta eliminada exitosamente";
+                    lbEstado.Visible = true;
+                    btnCrearTarjeta.Visible = true;
+                }
+
+                else
+                {
+                    lbResponse.Text = "Ocurrió un error";
+                }
+            }
+            catch (Exception exc)
+            {
+                string e = exc.Message;
+                lbResponse.Text = "Ocurrió un error";
+            }
+        }
+
+        protected void btnCrearTarjeta_Click(object sender, EventArgs e)
+        {
+            CreateNewCard();
+        }
+    }
+}
